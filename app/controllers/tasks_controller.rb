532764e9693ccace8a84e8aeadc98bdb4aba9d13
Elodie Mariton <elodie.mariton@hotmail.fr>
2022-05-17 15:32:12 +0200

    petite amelioration

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,18 +1,17 @@
 class TasksController < ApplicationController
+  before_action :set_tasks, only: [:show, :edit, :update, :destroy]
+
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
   end
 
   def edit
-    @task = Task.find(params[:id])
   end
 
   def update
-    @task = Task.find(params[:id])
     @task.update(task_params)
     redirect_to  tasks_path(@task)
   end
@@ -28,7 +27,6 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
     redirect_to tasks_path, status: :see_other
   end
@@ -37,5 +35,9 @@ class TasksController < ApplicationController
 
   def task_params
     params.require(:task).permit(:title, :details, :completed)
+  end
+
+  def set_tasks
+    @task = Task.find(params[:id])
   end
 end
