256e8076ef14d3cdb847a8f4a2d8a69817cd4225
Elodie Mariton <elodie.mariton@hotmail.fr>
2022-05-17 15:28:48 +0200

    exo1

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,17 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get 'tasks/:id', to: 'tasks#show', as: 'task'
+  get 'tasks', to: 'tasks#index'
+
+  get 'tasks/new', to: 'tasks#new'
+
+  post 'tasks', to: 'tasks#create'
+
+  delete 'tasks/:id', to: 'tasks#destroy'
+
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_new'
+  patch 'tasks/:id', to: 'tasks#update'
+
+
 end
